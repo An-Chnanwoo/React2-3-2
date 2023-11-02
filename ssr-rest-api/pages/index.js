89192986bbb443ede6dd4b822791fef2a8b6cbb8
An Chanwoo <sr4679@naver.com>
2023-11-02 12:49:56 +0900

    save

---
+++ ssr-rest-api/pages/index.js
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+import axios from 'axios'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export async function getServerSideProps(){
+  // const {data} = await axios.get(`${process.env.API_ENDPOINT}/api/04/users`);
+  const usersReq = await axios.get('https://api.rwnjs.com/01/users');
+
+  return{
+    props: {
+      users: usersReq.data,
+    },
+  };
+}
+
+export default function HomePage({users}) {
+  return (
+    <ul>s
+      {users.map((user) => (
+        <li key={user.id}>
+          <link href={`/users/${user.username}`} passHref>
+            {user.username}
+          </link>
+        </li>
+      ))}
+    </ul>
+  );
+}
